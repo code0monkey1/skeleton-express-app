ee5212aa2b8f1b5b61dc8ff2fd91b37fd4775a81
Chiranjeev Thomas <chiranjeevwork@gmail.com>
2023-09-20 09:35:49 +0000

    20 Sept 2023, 15:05

--- src/utils/SchemaValdation.ts
+++ src/utils/SchemaValdation.ts
@@ -1,15 +1,17 @@
 import { InvalidParamError } from '../errors/InvalidParamError';
 import { Validation } from './interfaces/validators/Validation';
 
-export class SchemaValidation implements Validation {
+export class SchemaValidation
+  implements Validation<{ name: string; age: string }>
+{
   constructor(private readonly schemaValidator: SchemaValidator) {}
-  validate(schemaObject: any): Error | null {
+  validate(schemaObject: any): Error | { name: string; age: string } {
     const isValid = this.schemaValidator.isValid(schemaObject);
 
     if (!isValid) {
       throw new InvalidParamError(schemaObject);
     }
 
-    return null;
+    return { name: 'c', age: 's' };
   }
 }
