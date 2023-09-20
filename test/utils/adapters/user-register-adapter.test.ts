33d06eea5461737076aeb67e5611eb016fccc9a9
Chiranjeev Thomas <chiranjeevwork@gmail.com>
2023-09-20 16:56:15 +0000

    20 Sept 2023, 22:26

---
+++ test/utils/adapters/user-register-adapter.test.ts
@@ -0,0 +1,46 @@
+import { UserRegisterAdapter } from '../../../src/utils/adapters/UserRegisterAdapter';
+import { UserRegister } from '../../../src/utils/interfaces/validators/RegisterUserParser';
+
+describe('user-register-adapter', () => {
+  describe('when data is valid', () => {
+    it('returns parsed data', () => {
+      //arrange
+      const sut = new UserRegisterAdapter();
+
+      const user: UserRegister = {
+        username: 'chiranjeev',
+        email: 'vonn@gmail.com',
+        password: 'watson',
+        repeat_password: 'watson',
+      };
+
+      //act
+
+      const result = sut.parse(user);
+
+      //assert
+
+      expect(result).toStrictEqual(user);
+    });
+  });
+  describe('when data is invalid', () => {
+    it('returns parsed data', () => {
+      //arrange
+      const sut = new UserRegisterAdapter();
+
+      const user: UserRegister = {
+        username: 'chiranjeev',
+        email: 'vonn@gmail.com',
+        password: 'watson',
+        repeat_password: 'wagson',
+      };
+
+      //act
+
+      const result = sut.parse(user);
+
+      //assert
+      console.log(result);
+    });
+  });
+});
