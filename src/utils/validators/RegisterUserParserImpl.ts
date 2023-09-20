3b8724dbf3ebaedf2756efc51d6e69108edb962e
Chiranjeev Thomas <chiranjeevwork@gmail.com>
2023-09-20 10:57:55 +0000

    20 Sept 2023, 16:27

--- src/utils/validators/RegisterUserParserImpl.ts
+++ src/utils/validators/RegisterUserParserImpl.ts
@@ -1,9 +1,28 @@
+// validationSchemas.ts
+import { z } from 'zod';
 import UserRegisterParser, {
   UserRegister,
 } from '../interfaces/validators/RegisterUserParser';
 
 class UserRegisterAdapter implements UserRegisterParser {
   parse(input: unknown): UserRegister {
-    throw new Error('Method not implemented.');
+    /* The `.refine()` method is used to add additional validation rules to the validation schema. In
+  this case, it is checking if the `password` field is equal to the `repeat_password` field. If
+  they are not equal, it will throw an error with the specified message ("Passwords don't match")
+  and the path of the error will be set to `['repeat_password']`. This allows for more specific
+  error messages and error paths when validating the data. */
+    const registerSchema = z
+      .object({
+        username: z.string().min(3).max(20).trim(),
+        email: z.string().email().trim(),
+        password: z.string().min(6),
+        repeat_password: z.string().min(6),
+      })
+      .refine((data) => data.password === data.repeat_password, {
+        message: " : Passwords don't match",
+        path: ['repeat_password'], // path of error
+      });
+
+    return registerSchema.parse(input);
   }
 }
